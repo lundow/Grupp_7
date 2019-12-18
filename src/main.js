cbdafe09deb028e6c1eb502c233fd1422de3ec8e
lundow <eriklundow@gmail.com>
2019-12-18 11:37:26 +0100

    add FrontEnd-folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const server = require("./server.js")
-
+const spotify = require("./apis/spotify.js")
 const port = 8000
 
 server.listen(port, () => {
   console.log("Server listening on port " + port)
+  spotify.getAccessToken();
 })
