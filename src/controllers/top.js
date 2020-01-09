901dd35f3ce722cd29006bfa743300fe23ce34e1
Henkomannen <henke94_olofsson@hotmail.com>
2020-01-09 15:47:13 +0100

    Added getting top albums, and top album cover

--- src/controllers/top.js
+++ src/controllers/top.js
@@ -1,4 +1,5 @@
 const lastFM = require("./../apis/lastFM.js")
+const spotify = require("./../apis/spotify.js")
 
 const getFavouriteTracks = async (params) => {
   const favourite_tracks = await lastFM.getFavouriteTracks(params.username, params.limit)
@@ -25,7 +26,35 @@ const getFavouriteArtists = async(params) => {
 	return artists
 }
 
+const getTopAlbums = async(params) => {
+	const top_albums = await lastFM.getTopAlbums(params.username, params.limit)
+	var albums = []
+	for(var i in top_albums) {
+		var album = top_albums[i]
+		var albumParams = {
+			"artist": album.artist.name,
+			"album": album.name
+		}
+		albums.push(albumParams)
+	}
+	return albums;
+}
+
+const getTopAlbumCovers = async(params) => {
+	const albums = await getTopAlbums(params)
+	const token = await spotify.fetchToken()
+
+	var top_albums = []
+	for(var i in albums) {
+		const albumInfo = await spotify.getAlbumInfo(albums[i].artist, albums[i].album, "album", 1)
+		top_albums.push(await albumInfo)
+	}
+	return top_albums
+}
+
 module.exports = {
 	getFavouriteTracks,
-	getFavouriteArtists
+	getFavouriteArtists,
+	getTopAlbums,
+	getTopAlbumCovers
 }
